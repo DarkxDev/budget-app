2cc9998e12fbe51c15aec9338beb5f982081458f
darkxdev <bryanfernandohurtado@gmail.com>
2023-05-29 16:00:05 -0500

    Added models tests using rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :entities, class_name: 'Entity', foreign_key: 'author_id'
-  has_many :groups, class_name: 'Group', foreign_key: "author_id"
+  has_many :groups, class_name: 'Group', foreign_key: 'author_id'
 
   validates :name, presence: true, length: { minimum: 3, maximum: 16 }
 end
